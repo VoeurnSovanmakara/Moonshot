223225b1deee8de0d72ff710032832130d2cbe0e
itsshinra <voeurnsovanmakara313@gmail.com>
2026-05-12 15:11:40 +0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by sovanmakara on 12/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoonshotTests/MoonshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotTests.swift
+//  MoonshotTests
+//
+//  Created by sovanmakara on 12/5/26.
+//
+
+import Testing
+@testable import Moonshot
+
+struct MoonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
