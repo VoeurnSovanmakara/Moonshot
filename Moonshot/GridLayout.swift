601f8a31ee7309b0efd33cdaf0cc996d75c029cf
itsshinra <voeurnsovanmakara313@gmail.com>
2026-05-13 17:17:43 +0700

    feat(): Complete Challenge

---
+++ Moonshot/GridLayout.swift
@@ -0,0 +1,18 @@
+//
+//  GridLayout.swift
+//  Moonshot
+//
+//  Created by sovanmakara on 13/5/26.
+//
+
+import SwiftUI
+
+struct GridLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridLayout()
+}
---
+++ Moonshot/ListLayout.swift
@@ -0,0 +1,18 @@
+//
+//  ListLayout.swift
+//  Moonshot
+//
+//  Created by sovanmakara on 13/5/26.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListLayout()
+}
